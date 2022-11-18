dce756a8dd6dfbd23ae3f71d7d7ab8e5190d00b7
Foodpunch <foodpunch@gmail.com>
2022-11-18 21:11:58 +0800

    Particles clean up
    -cleaned up particles by removing unused and unnecessary functions
    -added comments for things
    -all particles now scale with cellsize

--- Inc/Particles.h
+++ Inc/Particles.h
@@ -1,13 +1,21 @@
+/*!
+@file	  Particles.h
+@author	  Amadeus Chia (amadeusjinhan.chia@digipen.edu)
+@date     18/11/2022
+@brief    This header file contains the information about all particles in the game. The values of the particles are tweaked in the source file.
+________________________________________________________________________________________________________*/
 #pragma once
 #include "cprocessing.h"
-#include "ColorTable.h"
-#include <math.h>           //Remember to move to a utils.c!!
-#include <string.h>
-#include "EnemyStats.h"
+#include "ColorTable.h"     
+#include <math.h>           //some angle to vector calculations are needed
+#include <string.h>         //used for animating the particles
+#include "EnemyStats.h"     //for grabbing enemy data
 
+//====================== DEFINES ======================
 #define VECTOR_UP CP_Vector_Set(0,-1)
 #define VECTOR_DOWN CP_Vector_Set(0,1)
 #define PARTICLECOUNT 1001
+
 
 typedef struct Particle{
     float x;
@@ -15,32 +23,72 @@ typedef struct Particle{
     float xVelocity;
     float yVelocity;        
     float lifeTime;         //Current lifetime remaining for particle. Disappears when <0
-    float timeBeforeActive;
+    float timeBeforeActive; //Used to control the delay before a particle spawns.
     float cachedLifeTime;   //Used for calculating time particle has been alive for.
-    float size;             //NOTE!! FONT SIZE GOTTA BE BIG! OR CAN'T SEE SHITTT!! At least like, Min 15.f
+    float size;             //NOTE!! Size is normally set to cell size!
     BOOL isRGB;
+    BOOL isLoop;            //Manually set for certain particles in source
     CP_Vector force;        //force that is acting on particle. Cummulative and will be calculated in UpdateParticle.
     char* animString;
     CP_Color color;
-    //BOOL isActive;        //Returns false if the lifeTime is zero. If that's the case then just check against lifeTime?
-    float gravityScale;    //Maybe have gravity? Might be nice because it makes it curve
+    float gravityScale;    
 } Particle;
 
+/*
+@brief Create Particle will add the particle to the particle array for use.
 
+@param[in] Params : Refer to particle struct for full list of params.
+*/
 void CreateParticle(float xPos, float yPos, float lifeTime, float size,float gravityScale, CP_Color color,CP_Vector force,const char* animString,BOOL isRGB,float timeBeforeActive);
 
+/*
+@brief Updates a single particle's position and physics and deducts lifetime.
+@param[in] particlePointer pointer to a particle in the particleArray.
+*/
 void UpdateParticle(Particle* particlePointer);
+/*
+@brief Draws the particle if it still has lifetime.
+@param[in] particlePointer pointer to a particle in the particleArray.
+*/
 void DrawParticle(Particle* particlePointer);
-void ResetParticle(Particle* particlePointer);
+/*
+@brief Needs to be called every frame in update in order for all particles to be updated!
+*/
 void UpdateEffects(void);
+/*
+@brief Helper function for RGB values
+@param[in] Color,timeStep Returns a HSL color based on the time step given. 
+*/
 CP_Color LerpedHSLColor(CP_Color color,float timeStep);
-//Temp function for testing
+/*
+@brief Spawns a radial particle with no variance. 
+@param[in] Position is the particles x and y positions respectively.
+@param[in] Count is the number of particles that will be spawned. The angle of the particles is the 360/count. @param[in] Force controls how far and fast the particle moves.
+*/
 void RadialParticle(float x, float y,int particleCount,float force);
+/*
+@brief Spawns a radial particle with varied force and count. (set in source)
+@param[in] Position is the particles x and y positions respectively.
+*/
 void RadialParticleVaried(float x, float y);
-//Temp function for zombie despawn particle
+/*
+@brief Spawns the enemy's respective death particle. (Animation set in source)
+@param[in] Position is the particles x and y positions respectively.
+@param[in] Type refers to the zombies type in enemystats.c
+*/
 void ZombieDeathParticle(float x, float y,ZombieType type);
+/*
+@brief Spawns the enemy's spawn particle. (Animation set in source)
+@param[in] Position is the particles x and y positions respectively.
+*/
 void ZombieSpawnParticle(float x, float y);
-void ZombieToPlayerParticle(float x,float y,int health);
-
-//Move this to utils!!
+/*
+@brief Spawns the particle of the enemy moving towards the player.
+@param[in] Position is the particles x and y positions respectively.
+*/
+void ZombieToPlayerParticle(float x,float y);
+/*
+@brief Helper function for spawning particles in a circle. Returns the vector from the angle.
+@param[in] Angle is in degrees.
+*/
 CP_Vector AngleToVector(float degreeAngle);
--- Src/Particles.c
+++ Src/Particles.c
@@ -1,15 +1,22 @@
+/*!
+@file	  Particles.c
+@author	  Amadeus Chia (amadeusjinhan.chia@digipen.edu)
+@date     18/11/2022
+@brief    This source file for the handling of particles in game
+________________________________________________________________________________________________________*/
+
 #include "Particles.h"
-#include "Screenshake.h"
-#include "SoundManager.h"
-#include "WaveSystem.h"
-#include "Grid.h"
+#include "Screenshake.h"        
+#include "SoundManager.h"       
+#include "WaveSystem.h"         //for access to enemies
+#include "Grid.h"               //for grid coordinate conversions
 
 //Particle Array. Will use some form of Object Pooling.
 //ParticleCount defined in particles.h, 1000. 
 Particle particleArray[PARTICLECOUNT];
 particleIndex =0;
 
-//This is where I'll declare "anim strings for animation"
+//=========== PARTICLE ANIMATIONS ===========
 char* SparkleAnimString = "x+*\".";
 char* ZombieDeathAnimString = "ZZZZ    ZZZZ    ZZZZ    ZZZNNzznncu*\'`";
 char* LeaperDeathAnimString = "LLLL    LLLL    LLLL    LLL[[ii;;::*\'`";
@@ -21,7 +28,8 @@ char* ZombieAnim = "ZzZzZzZzZzZzZzZz";
 char* ZombieSpawnAnimString = "@Oo*\'";
 
 
-//Function that handles creating of particle.
+
+//Function that handles creating of particle. Calling this will add the particle to the array
 void CreateParticle(float xPos, float yPos, float lifeTime, float size,float gravityScale, CP_Color color,CP_Vector force,const char* animString,BOOL isRGB,float timeBeforeActive){
 
     Particle newParticle = {
@@ -37,13 +45,14 @@ void CreateParticle(float xPos, float yPos, float lifeTime, float size,float gra
         .force = force,
         .animString = animString,
         .isRGB = isRGB,
-        .timeBeforeActive = timeBeforeActive
+        .timeBeforeActive = timeBeforeActive,
+        .isLoop = FALSE
     };
 
+    //particle%PARTICLECOUNT will loop over to the start of the array
     particleArray[particleIndex%PARTICLECOUNT] = newParticle;
     particleIndex++;
 }
-
 
 //Function that updates a given particle. Used in Update effects forloop. Handles particle movement and lifetime.
 void UpdateParticle(Particle* particlePointer){
@@ -64,59 +73,45 @@ void UpdateParticle(Particle* particlePointer){
 
         particlePointer->lifeTime -= CP_System_GetDt();
         particlePointer->force.y += particlePointer->gravityScale * 9.81f*CP_System_GetDt();
-
-
+        //If the particle loops, it has infinite lifetime. 
+        if(particlePointer->isLoop && particlePointer->lifeTime <= 0){
+            particlePointer->lifeTime = particlePointer->cachedLifeTime;
+        }
     }
     else{
-        // ResetParticle(particlePointer);
+        // catch any errors here
     }
 
 }
 
-//Function that resets the particle. Called when particle's lifetime is 0 or less.
-//Called when lifetime < 0 
-void ResetParticle(Particle* particlePointer){
-
-    particlePointer->x = 0;
-    particlePointer->y = 0;
-    particlePointer->xVelocity=0;
-    particlePointer->yVelocity=0;
-    particlePointer->lifeTime =0;
-    particlePointer->timeBeforeActive=0;
-    particlePointer->force = CP_Vector_Zero();
-    //The other variables probably do not need to be reset.
-}
-
-//Temp function for testing the radial particles and it's anim
-//Maybe I'll make custom "particle functions" for things like spawning/despawning of enemies etc.
+//Spawns a radial particle with variance in angle, force and particle count
 void RadialParticleVaried(float x, float y){
-
+    //Values are hardcoded and tuned for the game.
     int randomParticleCount = CP_Random_RangeInt(8,30);
     //CP_Vector gravity = CP_Vector_Scale(VECTOR_DOWN,9.81f);
     for(short i = 0;i<(short)randomParticleCount;++i)
     {
         // float randomForceVariance = CP_Random_RangeFloat(8.f,15.f);
-        //Tweak the force here. For a proper circle, do not include variance. Will change this later
         float randomForceVariance = CP_Random_RangeFloat(2.f,8.f);
         float angle = (float)(360/randomParticleCount)*i+(CP_Random_Gaussian()*2.f);
         CP_Vector forceDirection = CP_Vector_Scale(AngleToVector(angle),randomForceVariance);
         //forceDirection = CP_Vector_Add(forceDirection,gravity);
-        CreateParticle(x,y,1.0f, 55.f,0.f, MENU_RED, forceDirection,SparkleAnimString,TRUE,0);
+        CreateParticle(x,y,1.0f, GetCellSize()/3,0.f, MENU_RED, forceDirection,SparkleAnimString,TRUE,0);
     }
 }
 
-
+//Spawns a radial particle with no variance. 
 void RadialParticle(float x, float y,int particleCount,float force){
     for(short i =0; i<particleCount;++i){
         float angle = (float)360/particleCount*i;
         CP_Vector forceDirection = CP_Vector_Scale(AngleToVector(angle),force);
-        CreateParticle(x,y,1.0f,55.f,0.f,MENU_RED,forceDirection,SparkleAnimString,TRUE,0);
+        CreateParticle(x,y,1.0f,GetCellSize()/3,0.f,MENU_RED,forceDirection,SparkleAnimString,TRUE,0);
     }
 }
 
+//Spawns the death particle of a zombie.
 void ZombieDeathParticle(float x, float y,ZombieType type){
-    switch (type)
-    {
+    switch (type){
     case ZOMBIE:
         CreateParticle(x,y,1.8f,GetCellSize()/3,0,MENU_RED,CP_Vector_Zero(),ZombieDeathAnimString,FALSE,0);
         trauma+=0.3f;
@@ -143,35 +138,33 @@ void ZombieDeathParticle(float x, float y,ZombieType type){
     }
 }
 
+//Spawns the spawn particle of zombies.
 void ZombieSpawnParticle(float x, float y){
-    CreateParticle(x,y,0.5f,120.f,0.1f,MENU_RED,CP_Vector_Zero(),ZombieSpawnAnimString,TRUE,0);
+    CreateParticle(x,y,0.5f,GetCellSize()/3,0.1f,MENU_RED,CP_Vector_Zero(),ZombieSpawnAnimString,TRUE,0);
 }
-void ZombieToPlayerParticle(float x,float y,int health){
+
+//Spawns the particle of the zombie moving to the player 
+void ZombieToPlayerParticle(float x,float y){
     CP_Vector dirToPlayer = CP_Vector_Set((CP_System_GetWindowWidth()*0.05f)-x,(GetGridPlayingArea()/2)+GetGridTopBuffer() - y);
     dirToPlayer = CP_Vector_Normalize(dirToPlayer);
-    CreateParticle(x,y,2.f,GetCellSize()*((float)health/(float)3),0,MENU_RED,dirToPlayer,ZombieAnim,TRUE,0);
+    CreateParticle(x,y,2.f,GetCellSize()/3,0,MENU_RED,dirToPlayer,ZombieAnim,TRUE,0);
     trauma+=0.5f;
 }
 
+//Handles the creation of particles with delay
 void CreateParticleEmission(float x, float y,int burstCount, float duration){
     for(short i=0; i<burstCount; ++i){
             for(short j =0; j<20;++j){
                 float angle = (float)360/20*j;
                 CP_Vector forceDirection = CP_Vector_Scale(AngleToVector(angle),8.f);
-                CreateParticle(x,y,1.0f,55.f,0.f,MENU_RED,forceDirection,SparkleAnimString,TRUE,(duration/(float)burstCount)*i);
+                CreateParticle(x,y,1.0f,GetCellSize()/3,0.f,MENU_RED,forceDirection,SparkleAnimString,TRUE,(duration/(float)burstCount)*i);
             }
-    }
-}
-
-void CreateMovementAnimation(int x, int y, int moveSpeed, float duration){
-    for(short i=0; i<moveSpeed; ++i){
-        CreateParticle(GridXToPosX(x),GridYToPosY(y),1.0f,GetCellSize(),0.f,MENU_GRAY,CP_Vector_Zero(),"<<<",FALSE,(duration*i));
     }
 }
 
 //Function that handles the animation and drawing of the particle to screen. Sets the color but not the alignment.
 void DrawParticle(Particle* particlePointer){
-    if(particlePointer->lifeTime >= 0 && particlePointer->timeBeforeActive <=0){
+    if((particlePointer->lifeTime >= 0)&& particlePointer->timeBeforeActive <=0){
 
         CP_Settings_TextAlignment(CP_TEXT_ALIGN_H_CENTER,CP_TEXT_ALIGN_V_MIDDLE);
         CP_Settings_TextSize(particlePointer->size);
@@ -181,6 +174,7 @@ void DrawParticle(Particle* particlePointer){
         float timeStep = elapsedLifeTime/particlePointer->cachedLifeTime;
         int totalFrames = (int)strlen(particlePointer->animString);
         int frameCount = (int)CP_Math_LerpFloat(0,(float)(totalFrames),timeStep);
+
         //Note : Lerp 2 values at one time for Razer RGB. (for future reference)
         
         if(particlePointer->isRGB){
@@ -192,25 +186,13 @@ void DrawParticle(Particle* particlePointer){
         char singleCharFrame[2];
         //memcpy is better than array=array.
         memcpy(&singleCharFrame,&particlePointer->animString[frameCount],1);
-        singleCharFrame[1]='\0';    //set the null char or drawtext will have undefined behaviour reading indefinitely
+        singleCharFrame[1]='\0';    //set the null char if not drawtext will have undefined behaviour reading indefinitely
         CP_Font_DrawText(*&singleCharFrame, particlePointer->x, particlePointer->y);
     }
 }
 
 //Function that is required to be called to update all particles. If particles don't work, check that this is in update loop.
 void UpdateEffects(void){
-    //Testing radial particle. Not sure if input should be checked here. Maybe it should be.
-    // if(CP_Input_MouseClicked()){
-    //     // ZombieDeathParticle(CP_Input_GetMouseX(),CP_Input_GetMouseY());
-    //     // NukeParticle(CP_Input_GetMouseX(),CP_Input_GetMouseY());
-    //     PlaySoundEx(TETROMINOEXPLODE,CP_SOUND_GROUP_SFX);
-    //     SendDamage(PosXToGridX(CP_Input_GetMouseX()),PosYToGridY(CP_Input_GetMouseY()),1);
-    // }
-    // if(CP_Input_MouseClicked())
-    // {
-    //     CreateParticleEmission(CP_Input_GetMouseX(),CP_Input_GetMouseY(),5,0.5f);
-    //     //  RadialParticle(CP_Input_GetMouseX(),CP_Input_GetMouseY(),20,8.f);
-    // }
     //Main loop for handling particle movement and rendering.
     //Important to loop through the whole array because the index wraps around.
     for(short i =0; i< PARTICLECOUNT; ++i){
@@ -229,7 +211,7 @@ CP_Color LerpedHSLColor(CP_Color color,float timeStep){
     return CP_Color_FromColorHSL(HSLColor);
 }
 
-//RANDOM MATH UTILS FUNCTION!! WILL PROBABLY MOVE IT TO A UTILS.C!!
+//Helper math function to convert angles to vector.
 CP_Vector AngleToVector(float degreeAngle){
     float angleRadians = CP_Math_Radians(degreeAngle);
     //Note, if the starting vector points up, the calculations will be different.
